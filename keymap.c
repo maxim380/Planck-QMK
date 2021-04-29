0bff7729780d0029ca0748426e37b108642879c7
Maxim Doolaard <m.doolaard@vericon.nl>
2021-04-29 14:13:00 +0200

    Switched functiond and pc control layers

--- keymap.c
+++ keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT_ortho_4x12(KC_Q,   KC_W,     KC_E,     KC_R,   KC_T,   KC_ESC,   KC_DEL,   KC_Y,   KC_U,     KC_I,     KC_O,     KC_P, 
                                 KC_A,   KC_S,     KC_D,     KC_F,   KC_G,   KC_TAB,   KC_ENT,   KC_H,   KC_J,     KC_K,     KC_L,     KC_SCLN, 
                                 KC_Z,   KC_X,     KC_C,     KC_V,   KC_B,   KC_LSFT,  KC_LCTL,  KC_N,   KC_M,     KC_COMM,  KC_DOT,   KC_SLSH, 
-                                MO(1),  KC_LGUI,  KC_LALT,  MO(4),  MO(3),  KC_BSPC,  KC_SPC,   MO(2),  KC_LEFT,  KC_UP,    KC_DOWN,  KC_RGHT),
+                                MO(1),  KC_LGUI,  KC_LALT,  MO(3),  MO(4),  KC_BSPC,  KC_SPC,   MO(2),  KC_LEFT,  KC_UP,    KC_DOWN,  KC_RGHT),
 
 	[_NUMPAD] = LAYOUT_ortho_4x12(KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, KC_ESC,  KC_DEL,   KC_7,     KC_8,  KC_9,    KC_KP_SLASH,    KC_NO,
                                 KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO, KC_TAB,  KC_BSPC,  KC_4,     KC_5,  KC_6,    KC_KP_ASTERISK, KC_NO, 
